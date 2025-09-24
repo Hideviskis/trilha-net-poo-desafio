ae48980b83b79c014d7fdbebda9d3804f9ac133d
Hideviskis <hideki.restani@outlook.com>
2025-09-23 23:02:03 -0300

    Desafio DIO POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone i = new Iphone("12982395243", "Iphone 15","1234", 8);
+Nokia n = new Nokia("12982395243", "WindowsPhone","1234", 4);
+
+//Iphone
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("Instagram");
+
+//Nokia
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("Whatsapp");
